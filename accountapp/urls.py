5cd2c79fa2915f1ce1c9ea21c7e7f2dcf0ef21bd
kyeongseoj <kyeongseojeon@likelion.org>
2021-08-01 17:46:09 +0900

    08_01 02

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import open_page, AccountCreateView
+from accountapp.views import open_page, AccountCreateView, AccountDetailView, AccountUpdateView
 
 app_name = 'accountapp'
 
@@ -12,5 +12,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
 
     path('create/', AccountCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
+    path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
 
 ]

e829ba02e4d70ef981d648c147c4a99d14b1173e
kyeongseoj <kyeongseojeon@likelion.org>
2021-08-01 18:52:32 +0900

    08_01 04

--- accountapp/views.py
+++ accountapp/views.py
@@ -12,18 +12,22 @@ from accountapp.models import Open_Page
 
 
 def open_page (request):
-    if request.method == 'POST':
+    if request.user.is_authenticated:
+        if request.method == 'POST':
 
-        temp = request.POST.get('open_page_input')
+            temp = request.POST.get('open_page_input')
 
-        new_open_page = Open_Page()
-        new_open_page.text = temp
-        new_open_page.save()
+            new_open_page = Open_Page()
+            new_open_page.text = temp
+            new_open_page.save()
 
-        return HttpResponseRedirect(reverse('accountapp:open'))
+            return HttpResponseRedirect(reverse('accountapp:open'))
+
+        else:
+            open_page_list = Open_Page.objects.all()
+            return render(request, 'accountapp/open_page.html', context={'open_page_list': open_page_list})
     else:
-        open_page_list = Open_Page.objects.all()
-        return render(request, 'accountapp/open_page.html', context={'open_page_list': open_page_list})
+        return HttpResponseRedirect(reverse('accountapp:login'))
 
 
 class AccountCreateView(CreateView):
@@ -46,6 +50,18 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:open')
     template_name = 'accountapp/update.html'
 
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super().post(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('accountapp:login'))
+
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super().post(request, *args, **kwargs)
+        else:
+            return  HttpResponseRedirect(reverse('accountapp:login'))
+
 
 class AccountDeleteView(DeleteView):
     model = User
@@ -53,7 +69,17 @@ class AccountDeleteView(DeleteView):
     success_url = reverse_lazy('accountapp:open')
     template_name = 'accountapp/delete.html'
 
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super().get(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('accountapp:login'))
 
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super().post(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('accountapp:login'))
 
 
 

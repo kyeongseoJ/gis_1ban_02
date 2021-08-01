9247b88c95c01eebc1d38d842224520c32c7e76c
kyeongseoj <kyeongseojeon@likelion.org>
2021-08-01 14:35:37 +0900

    07_31 04

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,12 +1,22 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
+from accountapp.models import Open_Page
+
 
 def open_page (request):
     if request.method == 'POST':
-        return render(request, 'accountapp/open_page.html',
-                      context={'text': ' POST METHOD!'})
+
+        temp = request.POST.get('open_page_input')
+
+        new_open_page = Open_Page()
+        new_open_page.text = temp
+        new_open_page.save()
+
+        return HttpResponseRedirect(reverse('accountapp:open'))
     else:
-        return render(request, 'accountapp/open_page.html',
-                      context={'text': ' GET METHOD!'})
+        open_page_list = Open_Page.objects.all()
+        return render(request, 'accountapp/open_page.html', context={'open_page_list': open_page_list})

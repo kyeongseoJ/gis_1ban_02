323cad2c0b3d1129b7bebdaa8c1f187e503a149d
kyeongseoj <kyeongseojeon@likelion.org>
2021-08-21 18:10:19 +0900

    08_21 04

--- projectapp/models.py
+++ projectapp/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+
 class Project(models.Model):
     name = models.CharField(max_length=20, null=False)
     description = models.CharField(max_length=200, null=False)
     image = models.ImageField(upload_to='project/', null=False)
 
-    created_at =models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name}'

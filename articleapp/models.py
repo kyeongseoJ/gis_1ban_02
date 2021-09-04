ebccf3e97de36faab396166dbda3815a6a08c311
kyeongseoj <kyeongseojeon@likelion.org>
2021-09-04 18:04:36 +0900

    09_04 04

--- articleapp/models.py
+++ articleapp/models.py
@@ -10,7 +10,7 @@ class Article(models.Model):
 
     project = models.ForeignKey(Project, on_delete=models.SET_NULL, related_name='article', null=True)
 
-    title = models.CharField(max_length=200, null=True)
+    title = models.CharField(max_length=20, null=True)
     image = models.ImageField(upload_to='article/', null=True)
     content = models.TextField(null=True)
 

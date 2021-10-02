9b5c49013d66596350640330c1344f592495db57
kyeongseoj <kyeongseojeon@likelion.org>
2021-10-02 18:00:35 +0900

    10.02 scd03

--- gis_1ban_02/settings/deploy.py
+++ gis_1ban_02/settings/deploy.py
@@ -1,21 +1,13 @@
 from .base import *
 
-env_list = dict()
-local_env = open(os.path.join(BASE_DIR, '.env'))
-while True:
-    line = local_env.readline()
-    if not line:
-        break
-    line = line.replace('\n', '')
-    start = line.find('=')
-    key = line[:start]
-    value = line[start+1:]
-    env_list[key] = value
+def read_secrets(secret_name):
+    file = open('/run/secrets/'+ secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+    return secret
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env_list['SECRET_KEY']
+SECRET_KEY = read_secrets('DJANGO_SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -30,8 +22,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
-        'USER': 'django',
-        'PASSWORD': 'password1234',
+        'USER': read_secrets('MARIADB_USER'),
+        'PASSWORD': read_secrets('MARIADB_PASSWORD'),
         'HOST': 'mariadb',
         'PORT': '3306',
     }

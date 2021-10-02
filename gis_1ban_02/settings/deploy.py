61cfb47bf5fa4f2320f2cd5df53925798e018231
kyeongseoj <kyeongseojeon@likelion.org>
2021-10-02 18:02:13 +0900

    10.02 scd05

--- gis_1ban_02/settings/deploy.py
+++ gis_1ban_02/settings/deploy.py
@@ -1,11 +1,13 @@
 from .base import *
 
+
 def read_secrets(secret_name):
     file = open('/run/secrets/'+ secret_name)
     secret = file.read()
     secret = secret.rstrip().lstrip()
     file.close()
     return secret
+
 
 SECRET_KEY = read_secrets('DJANGO_SECRET_KEY')
 
